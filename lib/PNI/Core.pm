7736b84a3f2d99e0a213aaa0109bcb0574f60acb
Gianluca Casati <fibo@cpan.org>
2011-12-23 23:46:55 +0000

    Import of FIBO/PNI-Core-0.06.2 from CPAN.
    gitpan-cpan-distribution: PNI-Core
    gitpan-cpan-version:      0.06.2
    gitpan-cpan-path:         FIBO/PNI-Core-0.06.2.tar.gz
    gitpan-cpan-author:       FIBO
    gitpan-cpan-maturity:     released

---
+++ lib/PNI/Core.pm
@@ -0,0 +1,35 @@
+package PNI::Core;
+
+our $VERSION = '0.06.2';
+
+1
+__END__
+
+=head1 NAME
+
+PNI::Core - Perl Node Interface core bundle
+
+=head1 SYNOPSIS
+
+    cpan PNI::Core
+
+=head1 DESCRIPTION
+
+Installs L<PNI>  and a default set of PNI nodes.
+
+This module also requires Perl version greater or equal than 5.10.1,
+
+since it is highly recommended by Perl Community to upgrade previous versions.
+
+Anyway if you want to install L<PNI> for older versions of Perl, you can do it
+
+    cpan PNI
+
+and then add any PNI::Node::* to your list.
+
+=head1 SEE ALSO
+
+L<PNI>
+
+=cut
+
